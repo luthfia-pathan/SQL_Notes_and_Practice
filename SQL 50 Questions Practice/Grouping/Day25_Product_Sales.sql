980e63bab119bb1ff6c790fa790b70c1428d4dd2
Luthfia Pathan <luthfiamdz@gmail.com>
2025-05-06 00:33:43 +0530

    Had completed Agg based Questions and Started Practicing Grouping based Questions

---
+++ SQL 50 Questions Practice/Grouping/Day25_Product_Sales.sql
@@ -0,0 +1,67 @@
+/*
+Product Sales Analysis III
+
+Table: Sales
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+
+Table: Product
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold. 
+If any product is bought multiple times in its first year, return all sales separately.
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+*/
+
+select product_id, year as first_year, quantity, price 
+from Sales 
+where (product_id, year) 
+in(
+    select product_id, min(year) 
+    from Sales 
+    group by product_id
+);
+-- As the product_id and year are unique, we can use them as a composite key to filter the first year of each product.
+-- The subquery selects the minimum year for each product_id, and the outer query selects the corresponding rows from the Sales table.
