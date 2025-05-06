dd856d1380fb0c93f0884b09c359544c784784cf
Luthfia Pathan <luthfiamdz@gmail.com>
2025-05-06 17:54:30 +0530

    Added till day 36 codes and Completed Adavced Functions Topics

---
+++ SQL 50 Questions Practice/Advance_Functions/Day30_Fix_Names.sql
@@ -0,0 +1,37 @@
+/*
+Fix Names in a Table
+
+Table: Users
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+*/
+
+select user_id, 
+concat(UCASE(substring(name,1,1)),Lower(substring(name,2,Length(name)))) as name 
+from Users 
+order by user_id;
+--  It's not index it is exact position of string value so 0 is not start position 1 is start position
