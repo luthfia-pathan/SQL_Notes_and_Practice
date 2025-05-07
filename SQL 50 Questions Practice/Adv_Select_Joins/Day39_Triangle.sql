afe0c619e1cb6f249b953bec053a003f565e92cb
Luthfia Pathan <luthfiamdz@gmail.com>
2025-05-07 15:41:16 +0530

    Completed Practicing Advanced Select and Joins

---
+++ SQL 50 Questions Practice/Adv_Select_Joins/Day39_Triangle.sql
@@ -0,0 +1,36 @@
+/*
+Triangle Judgement
+
+Table: Triangle
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+Report for every three line segments whether they can form a triangle.
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+select 
+x,y,z,
+case when (x+y) > z and (x+z) > y and (y+z) > x then 'Yes' else 'No' end as triangle
+from Triangle 
---
+++ SQL 50 Questions Practice/Adv_Select_Joins/Day40_Consecutive_no.sql
@@ -0,0 +1,45 @@
+/*
+Consecutive Numbers
+
+Table: Logs
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+
+Find all numbers that appear at least three times consecutively.
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
+
+Select Distinct num as ConsecutiveNums
+FROM (
+    SELECT num, 
+    LEAD(num,1) OVER() as next_num,
+    LEAD(num,2) OVER() as next_next_num
+    FROM Logs
+) as Temp
+where next_num = num and next_next_num = num
